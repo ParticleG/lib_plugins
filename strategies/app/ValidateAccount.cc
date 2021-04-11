ddd76803bd02c1776378a40fe1f39621f546ccf5
Particle_G <particle_g@outlook.com>
2021-04-11 20:41:24 +0800

    - Add mail support

---
+++ strategies/app/ValidateAccount.cc
@@ -0,0 +1,100 @@
+//
+// Created by Parti on 2021/4/10.
+//
+
+#include <mailio/message.hpp>
+#include <mailio/smtp.hpp>
+#include <plugins/Configurator.h>
+#include <strategies/actions.h>
+#include <strategies/app/ValidateAccount.h>
+#include <utils/crypto.h>
+#include <utils/misc.h>
+#include <utils/websocket.h>
+
+using namespace drogon;
+using namespace drogon_model;
+using namespace mailio;
+using namespace tech::plugins;
+using namespace tech::strategies;
+using namespace tech::utils;
+using namespace std;
+
+ValidateAccount::ValidateAccount() : _authMapper(app().getDbClient()), _infoMapper(app().getDbClient()) {}
+
+drogon::CloseCode ValidateAccount::fromJson(
+        const WebSocketConnectionPtr &wsConnPtr,
+        const Json::Value &request,
+        Json::Value &response
+) {
+    if (!(
+            request.isMember("data") && request["data"].isObject() &&
+            request["data"].isMember("email") && request["data"]["email"].isString() &&
+            request["data"].isMember("password") && request["data"]["password"].isString() &&
+            request["data"].isMember("username") && request["data"]["username"].isString()
+    )) {
+        response["type"] = "Warn";
+        response["reason"] = "Wrong format: Requires string type 'email', 'password' and 'username' in 'data'";
+    } else {
+        string email = request["data"]["email"].asString(),
+                password = request["data"]["password"].asString(),
+                username = request["data"]["username"].asString();
+        LOG_DEBUG << websocket::fromJson(request);
+        try {
+            if (!_authMapper.findBy(Criteria(Techmino::Auth::Cols::_email, CompareOperator::EQ, email)).empty()) {
+                response["type"] = "Warn";
+                response["reason"] = "Email already registered";
+            } else {
+                app().getDbClient()->execSqlSync(
+                        "insert into auth (email, password, validated) "
+                        "values ($1, crypt($2, gen_salt('bf', 10)), $3)",
+                        email, password, false
+                );
+                auto configurator = app().getPlugin<Configurator>();
+                auto tempAuth = _authMapper.findOne(Criteria(Techmino::Auth::Cols::_email, CompareOperator::EQ, email));
+                tempAuth.setAuthToken(crypto::panama::generateKey());
+                tempAuth.setAccessToken(crypto::panama::generateIV());
+                _authMapper.update(tempAuth);
+                Techmino::Info tempInfo;
+                tempInfo.setId(tempAuth.getValueOfId());
+                tempInfo.setEmail(email);
+                tempInfo.setUsername(username);
+                _infoMapper.insert(tempInfo);
+
+                Json::Value tempData;
+                tempData["email"] = email;
+                tempData["password"] = password;
+                tempData["expires"] = misc::fromDate(configurator->getEmailExpire());
+
+                message msg;
+                msg.from(mail_address(configurator->getMailName(), configurator->getMailAddress()));
+                msg.add_recipient(mail_address(username, email));
+                msg.subject("[Techmino] Validate your mailbox!");
+                msg.content("Click the link to validate your account: http://game.techmino.org:10026/tech/api/v1/app/register?"
+                            "id=" + to_string(tempAuth.getValueOfId()) +
+                            "&code=" + crypto::panama::encrypt(websocket::fromJson(tempData), tempAuth.getValueOfAuthToken(), tempAuth.getValueOfAccessToken()));
+
+                smtps conn(configurator->getHostName(), configurator->getPort());
+                conn.authenticate(configurator->getUsername(), configurator->getPassword(), smtps::auth_method_t::START_TLS);
+                conn.submit(msg);
+
+                response["action"] = static_cast<int>(actions::App::validateAccount);
+                response["type"] = "Self";
+                response["data"]["message"] = "Successfully created account. Please check mailbox in 15 minutes";
+            }
+        } catch (const orm::DrogonDbException &e) {
+            LOG_ERROR << "error:" << e.base().what();
+            response["type"] = "Error";
+            response["reason"] = "Internal error";
+            return CloseCode::kUnexpectedCondition;
+        } catch (smtp_error &e) {
+            LOG_ERROR << e.what();
+            response["type"] = "Error";
+            response["reason"] = e.what();
+        } catch (dialog_error &e) {
+            LOG_ERROR << e.what();
+            response["type"] = "Error";
+            response["reason"] = e.what();
+        }
+    }
+    return CloseCode::kNormalClosure;
+}
---
+++ strategies/app/ValidateAccount.h
@@ -0,0 +1,26 @@
+//
+// Created by Parti on 2021/4/10.
+//
+
+#pragma once
+
+#include <models/Auth.h>
+#include <models/Info.h>
+#include <strategies/base/MessageHandler.h>
+
+namespace tech::strategies {
+    class ValidateAccount : public MessageHandler {
+    public:
+        ValidateAccount();
+
+        drogon::CloseCode fromJson(
+                const drogon::WebSocketConnectionPtr &wsConnPtr,
+                const Json::Value &request,
+                Json::Value &response
+        ) override;
+
+    private:
+        drogon::orm::Mapper<drogon_model::Techmino::Auth> _authMapper;
+        drogon::orm::Mapper<drogon_model::Techmino::Info> _infoMapper;
+    };
+}
