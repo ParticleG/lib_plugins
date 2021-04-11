7e56f92f65ba12a54fe58623e86058418881ad8f
Particle_G <particle_g@outlook.com>
2021-04-11 20:47:36 +0800

    - use new image cache logic

--- strategies/user/GetUserInfo.cc
+++ strategies/user/GetUserInfo.cc
@@ -2,13 +2,16 @@
 // Created by Particle_G on 2021/2/17.
 //
 
+#include <strategies/actions.h>
 #include <strategies/user/GetUserInfo.h>
 #include <structures/User.h>
+#include <utils/crypto.h>
 
 using namespace drogon;
 using namespace drogon_model;
 using namespace tech::strategies;
 using namespace tech::structures;
+using namespace tech::utils;
 using namespace std;
 
 GetUserInfo::GetUserInfo() : _infoMapper(app().getDbClient()) {}
@@ -19,37 +22,35 @@ drogon::CloseCode GetUserInfo::fromJson(
         Json::Value &response
 ) {
     int64_t id = wsConnPtr->getContext<User>()->getInfo()->getValueOfId();
-    bool detailed = false;
+    std::string hash;
     if (request.isMember("data") && request["data"].isObject()) {
-        if (request["data"].isMember("id") && request["data"]["id"].isInt()) {
-            id = request["data"]["id"].asInt64();
+        if (request["data"].isMember("uid") && request["data"]["uid"].isInt()) {
+            id = request["data"]["uid"].asInt64();
         }
-        if (request["data"].isMember("detailed") && request["data"]["detailed"].isBool()) {
-            detailed = request["data"]["detailed"].asBool();
+        if (request["data"].isMember("hash") && request["data"]["hash"].isString()) {
+            hash = request["data"]["hash"].asString();
         }
     }
     try {
         auto info = _infoMapper.findOne(Criteria(Techmino::Info::Cols::__id, CompareOperator::EQ, id));
-        response["action"] = 1;
+        response["action"] = static_cast<int>(actions::User::getUserInfo);
         response["type"] = "Self";
-        response["id"] = info.getValueOfId();
-        response["email"] = info.getValueOfEmail();
-        response["username"] = info.getValueOfUsername();
+        response["data"]["uid"] = info.getValueOfId();
+        response["data"]["username"] = info.getValueOfUsername();
+        response["data"]["motto"] = info.getValueOfMotto();
 
-        if (detailed) {
-            response["motto"] = info.getValueOfMotto();
-            response["avatar"] = info.getValueOfAvatar();
+        if (!hash.empty() && hash != crypto::blake2b(info.getValueOfAvatar())) {
+            response["data"]["avatar"] = crypto::blake2b(info.getValueOfAvatar());
         }
-        return CloseCode::kNormalClosure;
     } catch (const UnexpectedRows &e) {
         LOG_WARN << e.what();
         response["type"] = "Warn";
         response["reason"] = "ID not found";
-        return CloseCode::kNormalClosure;
     } catch (const orm::DrogonDbException &e) {
         LOG_ERROR << "error:" << e.base().what();
         response["type"] = "Error";
         response["reason"] = "Internal error";
         return CloseCode::kUnexpectedCondition;
     }
+    return CloseCode::kNormalClosure;
 }
