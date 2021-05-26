15255ef482ea9492cb6d8c46c164e43b83c66b31
Particle_G <particle_g@outlook.com>
2021-05-26 18:10:01 +0800

    - Add support for linux and gcc

--- FindMailIO.cmake
+++ FindMailIO.cmake
@@ -2,6 +2,6 @@ if (MSVC)
     find_path(MAILIO_INCLUDE_DIR mailio PATHS ENV VCPKG_ROOT)
     find_file(MAILIO_LIB lib/mailio.lib PATHS ENV VCPKG_ROOT)
 else ()
-    find_path(MAILIO_INCLUDE_DIR mailio PATHS ENV VCPKG_ROOT)
-    find_file(MAILIO_LIB lib/libmailio.a PATHS ENV VCPKG_ROOT)
+    set(MAILIO_INCLUDE_DIR /mnt/c/dev/vcpkg/installed/x64-linux/include/) # TODO: Make it auto config
+    set(MAILIO_LIB /mnt/c/dev/vcpkg/installed/x64-linux/lib/libmailio.a)
 endif ()
