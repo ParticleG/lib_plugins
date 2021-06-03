74b1f379ebc64e78d7d5d1e00e3eddbc0e88b0b1
Particle_G <particle_g@outlook.com>
2021-06-04 02:45:04 +0800

    - Add "app" to generic infos

--- strategies/manage/GetGenericInfo.cc
+++ strategies/manage/GetGenericInfo.cc
@@ -2,6 +2,7 @@
 // Created by Particle_G on 2021/2/17.
 //
 
+#include <plugins/AppManager.h>
 #include <plugins/UserManager.h>
 #include <strategies/actions.h>
 #include <strategies/manage/GetGenericInfo.h>
@@ -19,11 +20,10 @@ drogon::CloseCode GetGenericInfo::fromJson(
         Json::Value &response
 ) {
     try {
-        auto userManager = app().getPlugin<UserManager>();
-
         response["action"] = static_cast<int>(actions::Manage::getGenericInfo);
         response["type"] = "Self";
-        response["data"] = userManager->parseInfo();;
+        response["data"] = app().getPlugin<UserManager>()->parseInfo();
+        response["data"]["app"] = app().getPlugin<AppManager>()->parseInfo();
         return CloseCode::kNormalClosure;
     } catch (const orm::DrogonDbException &e) {
         LOG_ERROR << "error:" << e.base().what();
