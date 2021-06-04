4a52583cfdb04b78388673d86afa528290123e59
Particle_G <particle_g@outlook.com>
2021-06-05 04:18:25 +0800

    - Make it fully support vcpkg on linux

--- FindMailIO.cmake
+++ FindMailIO.cmake
@@ -2,6 +2,6 @@ if (MSVC)
     find_path(MAILIO_INCLUDE_DIR mailio PATHS ENV VCPKG_ROOT)
     find_file(MAILIO_LIB lib/mailio.lib PATHS ENV VCPKG_ROOT)
 else ()
-    set(MAILIO_INCLUDE_DIR /mnt/c/dev/vcpkg/installed/x64-linux/include/) # TODO: Make it auto config
-    set(MAILIO_LIB /mnt/c/dev/vcpkg/installed/x64-linux/lib/libmailio.a)
+    set(MAILIO_INCLUDE_DIR /root/vcpkg/installed/x64-linux/include) # TODO: Make it auto config
+    set(MAILIO_LIB /root/vcpkg/installed/x64-linux/lib/libmailio.a)
 endif ()
